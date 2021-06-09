96625c95cb8247b442a810cc331c372a3b41580a
happy.k165131@gmail.com <ec2-user@ip-172-31-59-241.ec2.internal>
2021-06-09 13:41:36 +0000

    [Add]index,show,edit機能設置

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,40 @@
 class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to 'books_path(book.id)'
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  
+    book.destroy
+    redirect_to todolists_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
+  
